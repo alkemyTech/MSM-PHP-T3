6fd9537413f33bd3329df589dcb2d2fefcf5c489
lautiipereiraa <lautaropereirag@gmail.com>
2023-11-07 20:02:36 -0300

    SMP3-6
    SMP3-6

--- database/migrations/2023_11_07_230111_create_user_table.php
+++ database/migrations/2023_11_07_230111_create_user_table.php
@@ -14,13 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('role_id')->constrained();
             $table->timestamps();
+            $table->boolean('deleted')->default(false);
         });
     }
+    
+    
 
     /**
      * Reverse the migrations.
