431ca624ea80dfb3411a68195ef1147f699169d1
Gustavo Ferreyra <ferreyragustavo847@gmail.com>
2023-11-08 17:28:44 -0300

    SMP3-9

---
+++ database/migrations/2023_11_08_200255_create_fixed_terms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fixed_terms', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount')->notNull();
+            $table->foreignId('account_id')->constrained();
+            $table->double('interest')->notNull();
+            $table->double('total')->notNull();
+            $table->integer('duration')->notNull();
+            $table->timestamps();
+            $table->boolean('deleted')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fixed_terms');
+    }
+};
