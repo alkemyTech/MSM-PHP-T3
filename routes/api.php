b86ade712f87e1ac4efdcb165a7a40592ef009aa
Gustavo Ferreyra <ferreyragustavo847@gmail.com>
2023-11-15 16:53:39 -0300

    SMP3-12

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 Route::middleware(['api', 'auth:api'])->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('register', [AuthController::class, 'register'])->name('auth.registro')->withoutMiddleware(['auth:api']);
