244d1b7d89119222d025e3942ded892f13eb3960
lautiipereiraa <lautaropereirag@gmail.com>
2023-11-19 16:47:35 -0300

    SMP3-17 Corregido

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,29 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Models\Account;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Hash;
 
 class AccountController extends Controller
 {
-    public function store(Request $request){
-        $user = $request->user();
-        $account = new Account();
-        $account->cbu = Str::random(22);
-        $account->currency = $request->input('currency', 'ARS');
-        $account->balance = 0;
-        $account->transaction_limit = $request->input('transaction_limit', 300000);
-        $account->user_id = $user->id;
-      
-        if ($account->currency === 'USD') {
-          $account->transaction_limit = 1000;
-        } else {
-          $account->transaction_limit = $request->input('transaction_limit', 300000);
-        }
-      
-        $account->save();
-      
-        return $account;
+
+  public function generateCbu()
+  {
+      $cbu = '';
+      $cbu .= '00000123';
+      for ($i = 0; $i < 14; $i++) {
+          $cbu .= mt_rand(0, 9);
       }
+      return $cbu;
+  }
+
+  public function store(Request $request) {
+    $user = $request->user();
+    $currency = $request->get('currency', 'ARS');
+    $transactionLimit = $request->get('transaction_limit', 300000);
+
+    $account = new Account;
+    $account->cbu = $this->generateCbu();
+    $account->currency = $currency;
+    $account->balance = 0;
+
+    if ($currency === 'USD') {
+        $transactionLimit = 1000;
+    }
+
+    $account->transaction_limit = $transactionLimit;
+    $account->user_id = $user->id;
+    $account->save();
+
+    return $account;
+}
       
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FixedTermController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
