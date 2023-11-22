71cd8d1a700ea046a13140217be4e8ddef6267b8
Lautaro Pereira Garobi <lautaropereirag@gmail.com>
2023-11-22 19:09:52 -0300

    SMP3-23 Crear datos del modelo User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -20,12 +20,14 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $roleIds = Role::all()->pluck('id')->toArray();
+
         return [
             'name' => fake()->name(),
+            'last_name'=>fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'role_id' => fake()->randomElement($roleIds),
         ];
     }
 
